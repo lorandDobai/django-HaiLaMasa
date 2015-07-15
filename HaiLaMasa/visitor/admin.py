5b85c5a6db48a8960a66ffd1c7a7817b52ea9c63
Arno <arnobv@yahoo.com>
2015-07-15 12:56:57 +0300

    Added models to admin interface.

--- HaiLaMasa/visitor/admin.py
+++ HaiLaMasa/visitor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from visitor.models import Owner,Address,Contact,Gallery,Menu,Restaurant
 # Register your models here.
+admin.site.register(Owner)
+admin.site.register(Address)
+admin.site.register(Contact)
+admin.site.register(Gallery)
+admin.site.register(Menu)
+admin.site.register(Restaurant)
