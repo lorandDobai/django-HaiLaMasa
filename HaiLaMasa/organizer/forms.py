f7cac4496ad8239557f8435da7f1c1532e613afe
Arno <arnobv@yahoo.com>
2015-07-17 12:22:57 +0300

    App displays current user name.

--- HaiLaMasa/organizer/forms.py
+++ HaiLaMasa/organizer/forms.py
@@ -1,6 +1,11 @@
 
 from django import forms
+from visitor.models import Restaurant
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Nume utilizator', max_length=25)
     password = forms.CharField(label='Parola',widget = forms.PasswordInput())
+
+class RestaurantEditForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
