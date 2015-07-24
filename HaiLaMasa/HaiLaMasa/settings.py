49a91cbbaa1caf998c666f2b2dcaa2e873aeeb44
lorand <lorand.dobai@gmail.com>
2015-07-24 12:00:31 +0300

    static root settings

--- HaiLaMasa/HaiLaMasa/settings.py
+++ HaiLaMasa/HaiLaMasa/settings.py
@@ -106,5 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 LOGIN_URL = reverse_lazy('login')
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
